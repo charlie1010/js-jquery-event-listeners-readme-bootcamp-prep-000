982fde9a37df8fa11fcbc8778cbb49959bb14fc3
CHARLIE MCCORMICK <chamccor9692@gmail.com>
2018-05-21 16:20:28 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -19,7 +19,7 @@ function pressIt() {
 }
 
 function submitIt() {
-  $(document).on('submit', function() {
+  $('form').on('submit', function() {
     if ($("input:first").val() === "correct") {
       alert('your form is going to be submitted now')
       return
